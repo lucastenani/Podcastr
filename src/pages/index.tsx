79d7762daf9e54c6503796ea85b65f5ab4d69799
Lucas Tenani <lucas.tenani3@gmail.com>
2021-04-21 15:29:58 -0300

    Now that the Axios library has been deployed, I adapted the code for a more semantic way of requesting data from the API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@
 // during the day, only 3 calls to the API will be made to fetch the data.
 
 import { GetStaticProps } from 'next';
+import { api } from '../services/api';
 
 type Episode = {
   episodes: Array<{
@@ -26,8 +27,13 @@ export default function Home(props: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
 
   return {
     props: {
