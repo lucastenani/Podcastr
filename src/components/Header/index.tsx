a30899421faba36ccdc69f23f766fb21a34fe6aa
Lucas Tenani <lucas.tenani3@gmail.com>
2021-04-21 01:30:35 -0300

    The header component was created and styled

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,19 @@
-export function Header() {
-  return (
-    <header>
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+import styles from "./styles.module.scss";
 
+export function Header() {
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR,
+  });
+
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Podcastr Logo" />
+
+      <p>O melhor para você ouvir , sempre</p>
+
+      <span>{currentDate}</span>
     </header>
   );
 }
