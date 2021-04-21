14775360f35363d1bfdccfb55055a42a044ef541
Lucas Tenani <lucas.tenani3@gmail.com>
2021-04-21 14:49:13 -0300

    The Static site generators (SSG) method was applied, so that during the day, only 3 calls to the API will be made to retrieve the data, which greatly increases the performance of the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,23 @@
-export default function Home() {
+// SSG
+// Every 8 hours when a person accesses the page, a new version of that page will be generated, that is, during the day, only 3 calls to the API will be made to fetch the data.
+
+export default function Home(props) {
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
