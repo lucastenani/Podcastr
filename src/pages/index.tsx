923fba269b89c19da3a5112e4a6c83226ed2bacd
Lucas Tenani <lucas.tenani3@gmail.com>
2021-04-23 21:37:15 -0300

    I removed the description because it was not being used

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,6 @@ type Episode = {
   id: string;
   title: string;
   thumbnail: string;
-  description: string;
   members: string;
   duration: number;
   durationAsString: string;
@@ -132,7 +131,6 @@ export const getStaticProps: GetStaticProps = async () => {
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-      description: episode.description,
       url: episode.file.url,
     };
   })
